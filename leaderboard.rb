57a7c701a0da20cc6c1bfb57f29204222e6bfc3e
SasquatchShawn <srmiranda1@gmail.com>
2015-08-10 20:44:49 -0400

    Hash working with names, wins and losses, not rank

--- leaderboard.rb
+++ leaderboard.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def game_info
    [
     {
@@ -27,4 +28,50 @@ def game_info
   ]
 end
 
-# YOUR CODE HERE
+class Team
+  attr_accessor :name, :rank, :wins, :losses, :add_wins
+  def initialize(name, rank=0, wins=0, losses=0)
+    @name   = name
+    @rank   = rank
+    @wins   = wins
+    @losses = losses
+  end
+
+  def add_wins
+    @wins += 1
+  end
+
+  def add_losses
+    @losses += 1
+  end
+
+end
+
+teams = {}
+
+game_info.each do |info|
+  home_team = info[:home_team]
+  away_team = info[:away_team]
+
+  if !teams.has_key?(home_team)
+    home_team = Team.new(home_team)
+    teams[home_team.name] = home_team
+  end
+
+  if !teams.has_key?(away_team)
+    away_team = Team.new(away_team)
+    teams[away_team.name] = away_team
+  end
+end
+
+game_info.each do |info|
+ if info[:home_score] > info[:away_score]
+   teams[info[:home_team]].add_wins
+   teams[info[:away_team]].add_losses
+ else
+   teams[info[:home_team]].add_losses
+   teams[info[:away_team]].add_wins
+ end
+end
+
+puts teams
